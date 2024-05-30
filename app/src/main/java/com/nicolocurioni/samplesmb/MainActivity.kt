eeb2cbe24fe40b160e7f966355ad1cd5bd6e6430
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-05-30 17:57:45 +0200

    Added the business logic to handle the SMB connection

--- app/src/main/java/com/nicolocurioni/samplesmb/MainActivity.kt
+++ app/src/main/java/com/nicolocurioni/samplesmb/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.nicolocurioni.samplesmb
 
+import android.os.Build
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +15,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.nicolocurioni.samplesmb.shared.Secrets
 import com.nicolocurioni.samplesmb.ui.theme.SampleSMBTheme
+import jcifs.smb.NtlmPasswordAuthenticator
+import jcifs.smb.SmbFile
+import jcifs.smb.SmbFileInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
 
+@Suppress("DEPRECATION")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkPermissions()
+        setupSambaAccess()
+
         enableEdgeToEdge()
         setContent {
             SampleSMBTheme {
@@ -26,6 +43,67 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            }
+        }
+    }
+
+
+    // SMB stuff
+    private fun setupSambaAccess() {
+        val username = Secrets.sambaUsername.value
+        val password = Secrets.sambaPassword.value
+        val domain = ""
+        val host = Secrets.sambaHost.value
+        val share = Secrets.sambaShare.value
+
+        val smbURL = URL("$host")
+
+        val auth = NtlmPasswordAuthenticator(username, password)
+        val smbDirectory = SmbFile(smbURL)
+
+        try {
+            if (smbDirectory.exists() && smbDirectory.isDirectory) {
+                val files = smbDirectory.listFiles()
+
+                files.forEach { file ->
+                    println("File: ${file.name}")
+
+                    if (file.isFile) {
+                        //downloadFile(file, Environment.DIRECTORY_DOCUMENTS + "sample-smb")
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun downloadFile(file: SmbFile, localPath: String) {
+        try {
+            SmbFileInputStream(file).use { fileInput ->
+                FileOutputStream(File(localPath)).use { fileOutput ->
+                    val buffer = ByteArray(4096)
+                    var byteRead: Int
+
+                    while (fileInput.read(buffer).also { byteRead = it } != -1) {
+                        fileOutput.write(buffer, 0, byteRead)
+                    }
+
+                    fileOutput.flush()
+                    println("File downloaded: $localPath")
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
